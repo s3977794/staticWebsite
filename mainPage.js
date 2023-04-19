25673b05a9299e70f7f6a5f67673505c504f32d3
s3977794 <s3977794@rmit.edu.vn>
2023-04-19 21:18:48 +0700

    Small changed

--- mainPage.js
+++ mainPage.js
@@ -1,3 +1,17 @@
+// <!-- /* RMIT University Vietnam
+// Course: COSC2430 Web Programming
+// Semester: 2023A
+// Assessment: Assignment 1
+// Author: Le Dang Quang
+// ID: s3977794
+// Acknowledgement: 
+// icon:https://fontawesome.com/
+// books:
+// https://markmanson.net/best-books/fiction#:~:text=The%2047%20Best%20Fiction%20Books%20of%20All%20Time,to%20Napoleon%20himself.%20The%20modern%20epic.%20More%20items
+// https://reedsy.com/discovery/blog/best-nonfiction-books
+// https://www.goodhousekeeping.com/life/entertainment/g26143680/best-romance-novels/
+// https://www.ereads.com/best-science-fiction-books/#:~:text=Best%20Science%20Fiction%20Books%201%20Dune%20by%20Frank,Sheep%3F%20by%20Philip%20K.%20Dick%20...%20More%20items
+// stationary:https://www.fahasa.com/ */ -->
 
 const toggleBtn = document.getElementById('toggleBtn');
 const dropDownContent = document.getElementById('dropDownContent');
@@ -10,4 +24,30 @@ toggleBtn.addEventListener('click', () => {
     }
 })
 
+const minus = document.getElementById("minus");
+const plus = document.getElementById("plus");
+const quantity = document.getElementById("Quantity");
 
+let number = 1
+plus.addEventListener("click", () => {
+    number++;
+    quantity.innerText = number;
+    console.log(number);
+});
+
+minus.addEventListener("click", () => {
+    if(number > 1){
+        number--;
+        quantity.innerText = number;
+    }
+    console.log(number)
+})
+
+
+function Added() {
+    alert("Your item has been added to cart!");
+}
+
+function noty() {
+    alert("Our customer care will contact you later. Thank you!");
+}
